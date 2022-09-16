93ab31f5e7d12396c0ea2f8a320c3f675048ca72
giancarlo <giancarlopsk@gmail.com>
2022-09-16 11:58:48 +0200

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+
+const analyzeBundleEnabled = process.env.ANALYZE === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer({
+  enabled: analyzeBundleEnabled
+})(nextConfig);
